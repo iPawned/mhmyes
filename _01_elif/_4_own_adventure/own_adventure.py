767e538569c9bcd13949130e45671ce0a30a9c44
wifhwo <aryan.chess18@gmail.com>
2023-09-07 01:32:04 +0000

    efwf

--- _01_elif/_4_own_adventure/own_adventure.py
+++ _01_elif/_4_own_adventure/own_adventure.py
@@ -4,3 +4,6 @@ from tkinter import simpledialog, messagebox, Tk
 #  path of the plot.
 #  Use pop-ups, if statements, and your imagination to make an interesting
 #  story
+quest1 = simpledialog.askstring(title="Question 1:", prompt="Do you want to go on an adventure?")
+if quest1 == "No":
+    messagebox.showinfo(title="Well then,")
