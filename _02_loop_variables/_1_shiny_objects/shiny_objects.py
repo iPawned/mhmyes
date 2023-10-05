3cd598611328b13ec6a6156d16c1ad4dccb2a411
qehfdwiu <aryan.chess18@gmail.com>
2023-10-05 01:28:47 +0000

    dyhrtjtfj

--- _02_loop_variables/_1_shiny_objects/shiny_objects.py
+++ _02_loop_variables/_1_shiny_objects/shiny_objects.py
@@ -13,10 +13,12 @@ def play_mister_zee():
 
 def many_shiny_objects():
     # TODO 1) Call the method above to play Mister Zee
-
+    play_mister_zee()
     # TODO 2) Ask the user how many shiny objects they want
-
+    number = simpledialog.askinteger(title="Shiny Objects", prompt="How many shiny objects do you want?")
     # TODO 3) Play the sound that many times
+    for i in range(number):
+        play_mister_zee()
 
     pass
 
@@ -25,7 +27,3 @@ if __name__ == '__main__':
     window = Tk()
     window.withdraw()
     many_shiny_objects()
-play_mister_zee()
-number = simpledialog.askinteger(title="Shiny Objects", prompt="How many shiny objects do you want?")
-for i in range: number
-play_mister_zee()
